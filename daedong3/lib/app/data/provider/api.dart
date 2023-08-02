62c9d16db0a85ba82b3ce894a26b90e1dc4ca073
hoooonshub <yhn515@naver.com>
2023-08-03 02:14:02 +0900

    Api Class

--- daedong3/lib/app/data/provider/api.dart
+++ daedong3/lib/app/data/provider/api.dart
@@ -1,6 +1,20 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class Api{
-  
+var createChatRoomUri = Uri.parse('http://13.125.221.81:8080/daedong/menu/createchatroom');
+
+class Api {
+
+  createChatRoom() async{
+    try{
+      var response = await http.post(createChatRoomUri);
+      if(response.statusCode == 200){
+
+      }else if{
+
+      }else{
+
+      }
+    }
+  }
 }
