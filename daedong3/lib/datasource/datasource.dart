cc4d4078ff8bc751011af0325c66f6b05d5cf35b
hoooonshub <yhn515@naver.com>
2023-08-30 16:15:02 +0900

    0830 API 추가

--- daedong3/lib/datasource/datasource.dart
+++ daedong3/lib/datasource/datasource.dart
@@ -16,36 +16,61 @@ class DataSource {
   }
 
   // 사용자 질문
-  Future<String> answerQuestion() async{
+  Future<String> answerQuestion(Question question) async{
     final responseAnswerQuestion = await http
         .post(Uri.parse('http://13.125.221.81:8080/daedong/chatroom/question'),
-              body: Question(id: /*id*/, question: /*question*/).toJson();
-        );
+              body: question.toJson());
     return jsonDecode(responseAnswerQuestion.body)
         .toString();
   }
 
   // 잘못된 정보 수정 요청
+  Future<String> requestModifyInfo() async{
+    final requestModifyInfo = await http
+        .post(Uri.parse('http://13.125.221.81:8080/daedong/chatroom/modify'),
+              body: /* id, contextIndex, text */ );
+    return jsonDecode(requestModifyInfo.body)
+        .toString();
+  }
 
   // 채팅방 제목 수정
-  Future<String> updateTitle() async{
-
+  Future<String> updateTitle(ChatRoom chatRoom, String title) async{
     final responseUpdateTitle = await http
-        .post(Uri.parse('http://13.125.221.81:8080/daedong/updatetitle/${chatTitle}'),
-              // body: ChatRoom().toJson;
-        );
+        .post(Uri.parse('http://13.125.221.81:8080/daedong/updatetitle/$title'),
+                body: chatRoom.toJson());
     return jsonDecode(responseUpdateTitle.body)
         .toString();
   }
 
   // 채팅방 삭제
-  Future<String> deleteChatRoom() async{
+  Future<String> deleteChatRoom(ChatRoom chatRoom) async{
     final responseDeleteChatRoom = await http
         .post(Uri.parse('http://13.125.221.81:8080/daedong/deletechatroom'),
-              body: ChatRoom().toJson());
+              body: chatRoom.toJson());
     return jsonDecode(responseDeleteChatRoom.body)
         .toString();
   }
 
-  // pastList
+  // 로그인한 사용자의 최근 채팅방
+  Future<ChatRoom> latestChatRoom(String userId) async{
+    final latestChatRoom = await http
+        .get(Uri.parse('http://13.125.221.81:8080/daedong/$userId'));
+    return jsonDecode(latestChatRoom.body);
+  }
+
+  // 사용자가 선택한 채팅방
+  Future<ChatRoom> chosenChatRoom(String chatRoomId) async{
+    final chosenChatRoom = await http
+        .get(Uri.parse('http://13.125.221.81:8080/daedong/chatroom/$chatRoomId'));
+    return jsonDecode(chosenChatRoom.body);
+  }
+
+  // 과거 채팅 내용 조회
+  Future<dynamic> pastChatList(String userId) async{
+    final pastChatList = await http
+        .get(Uri.parse('http://13.125.221.81:8080/daedong/pastList/$userId'));
+    return jsonDecode(pastChatList.body);
+  }
+
+
 }
--- daedong3/lib/repository/repository.dart
+++ daedong3/lib/repository/repository.dart
@@ -1,7 +1,17 @@
 import '../datasource/datasource.dart';
+import '../model/question.dart';
+import '../model/user.dart';
 
 class Repository{
   final DataSource _dataSource = DataSource();
 
+  // 채팅 생성
+  Future<String> createChatRoom(User user) async {
+    return _dataSource.createChatRoom(user);
+  }
 
+  // 사용자 질문
+  Future<String> answerQuestion(Question question) async {
+    return _dataSource.answerQuestion(question);
+  }
 }
