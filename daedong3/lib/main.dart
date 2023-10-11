72e9ed333d6f9d778b5a0827cf2a725d46d7da56
hoooonshub <yhn515@naver.com>
2023-10-11 10:24:57 +0900

    Revert "Merge branch 'main' into brchHoon"
    This reverts commit ba35cf072a00a7785ad5c27bd32830a41ee25cd0, reversing
    changes made to 03446b9c8fed546f74cfd088863c164e6ce3c872.

--- daedong3/lib/main.dart
+++ daedong3/lib/main.dart
@@ -1,15 +1,11 @@
-import 'package:daedong3/view/login.dart';
+import 'package:daedong3/view/home_page.dart';
 import 'package:daedong3/viewmodel/hamburger_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:daedong3/view/home_page.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'personal_information.dart';
 
 
 void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(MultiProvider(
       providers: [ChangeNotifierProvider(create: (_)=>HamburgerViewModel()),],
       child: MyApp()));
@@ -19,31 +15,14 @@ class MyApp extends StatelessWidget {
   @override
 
   Widget build(BuildContext context) {
-    FlutterNativeSplash.remove();
     return MaterialApp(
         theme: ThemeData(
           appBarTheme: AppBarTheme(backgroundColor: Colors.lightBlueAccent)
         ),
         debugShowCheckedModeBanner: false,
-      //home: HomePage(PersonalInformation('로그인 필요','')),
-      home : Login(),
-      initialRoute: '/login',
-      onGenerateRoute: _getRoute,
-
+      home: HomePage(PersonalInformation('로그인 필요','')),
     );
 
   }
-}
-//navigator 사용위한 루트
-Route<dynamic>? _getRoute(RouteSettings settings){
-  if(settings.name != '/login'){
-    return null;
-  }
-
-  return MaterialPageRoute<void>(
-      settings: settings,
-      builder: (BuildContext context) => Login(),
-      fullscreenDialog: true,
-  );
 }
 
--- daedong3/lib/repository/repository.dart
+++ daedong3/lib/repository/repository.dart
@@ -6,92 +6,130 @@ import '../model/chat_room.dart';
 import '../model/login.dart';
 import '../model/past_chat.dart';
 import '../model/question.dart';
+import '../model/signup.dart';
 import '../model/user.dart';
 import 'package:http/http.dart' as http;
+import '../model/user_update.dart';
 
-class Repository{
-  // 새 채팅 생성
-  Future<String> createChatRoom(User user) async{
+class Repository {
+  // API 순서대로 메소드 작성
+
+  // 사용자 질문
+  Future<String> answerQuestion(Question question) async {
     try {
-      final response = await http
-          .post(
-          Uri.parse('http://13.125.221.81:8080/daedong/menu/createchatroom'),
-          body: {user.toJson()});
-      if(response.statusCode == 200){
-        return jsonDecode(response.body)
-            .toString();
-      }else{ // 에러 처리 미흡. 예하 동일
-        throw Exception('Request Error: ${response.statusCode}');
-      }
-    } catch(e){
+      final response = await http.post(
+          Uri.parse('http://13.209.50.197:8080/daedong/chatroom/question'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(question.toJson()));
+      return response.body; // response가 String이어서 그대로 return
+    } catch (e) {
       throw Exception('Error: $e');
     }
   }
 
-  // 사용자 질문
-  Future<String> answerQuestion(Question question) async{
-    try{
-      final response = await http
-          .post(Uri.parse('http://13.125.221.81:8080/daedong/chatroom/question'),
-          body: question.toJson());
-      if(response.statusCode == 200){
-        return jsonDecode(response.body)
-            .toString();
-      }else{
-        throw Exception('Request Error: ${response.statusCode}');
-      }
-    }catch(e){
+  // 로그인한 사용자의 최근 채팅방
+  Future<ChatRoom> latestChatRoom(String userId) async {
+    try {
+      final response = await http.get(
+        Uri.parse('http://13.209.50.197:8080/daedong/$userId'),
+      );
+      return ChatRoom.fromJson(jsonDecode(response.body));
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  // 사용자가 선택한 채팅방
+  Future<ChatRoom> chosenChatRoom(String chatRoomId) async {
+    try {
+      final response = await http.get(
+          Uri.parse('http://13.209.50.197:8080/daedong/chatroom/$chatRoomId'));
+      return ChatRoom.fromJson(jsonDecode(response.body));
+    } catch (e) {
       throw Exception('Error: $e');
     }
   }
 
   // 잘못된 정보 수정 요청
-  Future<String> requestModifyInfo() async{
-    try{
-      final response = await http
-          .post(Uri.parse('http://13.125.221.81:8080/daedong/chatroom/modify'),
-          body: null/* id, contextIndex, text */ );
-      if(response.statusCode == 200){
-        return jsonDecode(response.body)
-            .toString();
-      }else{
-        throw Exception('Request Error: ${response.statusCode}');
+  Future<bool> requestModifyInfo(InfoModifyRequest infoModifyRequest) async {
+    // bool 값으로 리턴
+    try {
+      final response = await http.post(
+          Uri.parse('http://13.209.50.197:8080/daedong/chatroom/modify'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(infoModifyRequest.toJson()));
+      if (response.body == "success") {
+        return true;
+      } else {
+        return false;
       }
-    }catch(e){
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  // 새 채팅 생성
+  Future<String> createChatRoom(User user) async {
+    try {
+      final response = await http.post(
+          Uri.parse('http://13.209.50.197:8080/daedong/menu/createchatroom'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(user.toJson()));
+      return response.body;
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  // 과거 채팅 내용 조회
+  Future<List<PastChat>> pastChatList(String userId) async {
+    try {
+      final response = await http
+          .get(Uri.parse('http://13.209.50.197:8080/daedong/pastList/$userId'));
+      List<dynamic> body =
+      jsonDecode(response.body); // response 값 body에 디코딩해서 담기
+      List<PastChat> chatList = body
+          .map((dynamic item) => PastChat.fromJson(item))
+          .toList(); // PastChat 객체로 변환해서 리스트 형태로 리턴
+      return chatList;
+    } catch (e) {
       throw Exception('Error: $e');
     }
   }
 
   // 채팅방 제목 수정
-  Future<String> updateTitle(ChatRoom chatRoom, String title) async{
-    try{
-      final response = await http
-          .post(Uri.parse('http://13.125.221.81:8080/daedong/updatetitle/$title'),
-          body: chatRoom.toJson());
-      if(response.statusCode == 200){
-        return jsonDecode(response.body)
-            .toString();
-      }else{
-        throw Exception('Request Error: ${response.statusCode}');
+  Future<bool> updateTitle(ChatRoom chatRoom, String newChatTitle) async {
+    // bool 값으로 리턴
+    try {
+      final response = await http.post(
+          Uri.parse(
+              'http://13.209.50.197:8080/daedong/updatetitle/$newChatTitle'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(chatRoom.toJson()));
+      if (response.body == "success") {
+        return true;
+      } else {
+        return false;
       }
-    }catch(e){
+    } catch (e) {
       throw Exception('Error: $e');
     }
   }
 
   // 채팅방 삭제
-  Future<String> deleteChatRoom(ChatRoom chatRoom) async{
-    try{
-      final response = await http
-          .post(Uri.parse('http://13.125.221.81:8080/daedong/deletechatroom'),
-          body: chatRoom.toJson());
-      if(response.statusCode == 200){
-        return jsonDecode(response.body)
-            .toString();
-      }else{
-        throw Exception('Request Error: ${response.statusCode}');
+  Future<bool> deleteChatRoom(ChatRoom chatRoom) async {
+    // bool 값으로 리턴
+    try {
+      final response = await http.post(
+          Uri.parse('http://13.209.50.197:8080/daedong/deletechatroom'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(chatRoom.toJson()));
+      if (response.body == "success") {
+        return true;
+      } else {
+        return false;
       }
-    }catch(e){
+    } catch (e) {
       throw Exception('Error: $e');
     }
   }
@@ -130,39 +168,59 @@ class Repository{
       } else {
         return false;
       }
-    }catch(e){
+    } catch (e) {
       throw Exception('Error: $e');
     }
   }
 
-  // 사용자가 선택한 채팅방
-  Future<ChatRoom> chosenChatRoom(String chatRoomId) async{
-    try{
-      final response = await http
-          .get(Uri.parse('http://13.125.221.81:8080/daedong/chatroom/$chatRoomId'));
-      if(response.statusCode == 200){
-        return jsonDecode(response.body);
-      }else{
-        throw Exception('Request Error: ${response.statusCode}');
+  // 회원 탈퇴
+  Future<bool> deleteUser(User user) async {
+    try {
+      final response = await http.post(
+          Uri.parse('http://13.209.50.197:8080/daedong/deleteuser'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(user.toJson()));
+      if (response.body == "success") {
+        return true;
+      } else {
+        return false;
       }
-    }catch(e){
+    } catch (e) {
       throw Exception('Error: $e');
     }
   }
 
-  // 과거 채팅 내용 조회
-  Future<dynamic> pastChatList(String userId) async{
-    try{
-      final response = await http
-          .get(Uri.parse('http://13.125.221.81:8080/daedong/pastList/$userId'));
-      if(response.statusCode == 200){
-        return jsonDecode(response.body);
-      }else{
-        throw Exception('Request Error: ${response.statusCode}');
+  // 로그아웃
+  Future<bool> logout() async {
+    try {
+      final response = await http.post(
+          Uri.parse('http://13.209.50.197:8080/daedong/logout')
+      );
+      if (response.body == "success") {
+        return true;
+      } else {
+        return false;
       }
-    }catch(e){
+    } catch (e) {
       throw Exception('Error: $e');
     }
   }
 
+  // 회원가입
+  Future<bool> signup(SignUp signUp) async{
+    try {
+      final response = await http.post(
+          Uri.parse('http://13.209.50.197:8080/daedong/join'),
+        headers: {"Content-Type" : "application/json"},
+        body: jsonEncode(signUp.toJson())
+      );
+      if (response.body == "success") {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
 }
