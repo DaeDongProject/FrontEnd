2aada106bfcf2e3f7efff33bd6d74d248c3af983
Booreung <120548686+Booreung@users.noreply.github.com>
2023-11-08 18:59:20 +0900

    [이상함]

--- daedong3/lib/view/chat/chat_message.dart
+++ daedong3/lib/view/chat/chat_message.dart
@@ -2,101 +2,78 @@ import 'package:daedong3/model/chat_item.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
-
-  final Animation<double> animation;
+  //final List<String> messages;
+  //final String txt;
+  // final Animation<double> animation;
   final bool isMe;
-  //final ChatItem chatItem;
-  final List<ChatItem> chatItems;
+  // final ChatItem chatItem;
+  final String message;
 
   const ChatMessage(
-      this.chatItems, this.isMe ,{
-        required this.animation,
+      this.message, this.isMe ,{
+        // required this.animation,
         Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return  Padding(
+
       padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8 ),
-      child: Column(
-        children: chatItems.map((chatItem) {
-          return buildChatItem(chatItem);
-        }).toList()),
-    );
-  }
-  Widget buildChatItem(ChatItem chatItem){
-    return FadeTransition(
-        opacity: animation,
-        child: SizeTransition(
-          sizeFactor: animation,
-          axisAlignment: -1.0,
-          child: Row(
-            mainAxisAlignment: isMe ?  MainAxisAlignment.end : MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                width:  16,
-              ),
-              Flexible(
-                  child: Column(
-                    crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-                    children: [
-                      Text( "나" , style: TextStyle(fontWeight: FontWeight.bold),),
-                      // for (String message in messages)
-                      Container(
-                        child: Text(chatItem.question,style: TextStyle(color: isMe ? Colors.white : Colors.black, fontWeight: FontWeight.w500),),
-                        padding: EdgeInsets.all(7.0),
-                        decoration: BoxDecoration(
-                          color: Colors.lightBlueAccent ,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(12),
-                            topRight: Radius.circular(12),
-                            bottomRight:  Radius.circular(0),
-                            bottomLeft: Radius.circular(12) ,
-                          ),
-                          border : Border.all(color: isMe ? Colors.lightBlueAccent : Colors.grey.shade200,)
-                        ),
-                      ),
-                      if (chatItem.answer != "")...{
-                        Text("대동이", style: TextStyle(fontWeight: FontWeight.bold),),
-                        Container(
-                          child: Text(chatItem.answer, style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),),
-                          padding: EdgeInsets.all(7.0),
-                          decoration: BoxDecoration(
-                            color:  Colors.grey.shade400,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(12),
-                              topRight: Radius.circular(12),
-                              bottomRight:  Radius.circular(12),
-                              bottomLeft: Radius.circular(0),
-                            ),
-                            border: Border.all(color: Colors.grey.shade200,),
-                          ),
-                        ),
-                      }
-                      else...{SizedBox.shrink()}
-                        // 답변이 비어 있지 않은 경우에만 표시
-                        // Container(
-                        //   child: Text(chatItem.answer, style: TextStyle(color: isMe ? Colors.white : Colors.black, fontWeight: FontWeight.w500),),
-                        //   padding: EdgeInsets.all(7.0),
-                        //   decoration: BoxDecoration(
-                        //     color: isMe ? Colors.lightBlueAccent : Colors.grey.shade400,
-                        //     borderRadius: BorderRadius.only(
-                        //       topLeft: Radius.circular(12),
-                        //       topRight: Radius.circular(12),
-                        //       bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
-                        //       bottomLeft: isMe ? Radius.circular(12) : Radius.circular(0),
-                        //     ),
-                        //     border: Border.all(color: isMe ? Colors.lightBlueAccent : Colors.grey.shade200,),
-                        //   ),
-                        // ),
-                    ],
-                  )
-              ),
-            ],
+
+      // child: FadeTransition(
+      //   opacity: ,
+      //   child: SizeTransition(
+      //     // sizeFactor: animation,
+      //     axisAlignment: -1.0,
+      child: Row(
+        mainAxisAlignment: isMe ?  MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            width:  16,
           ),
-        ),
-      );
+          Flexible(
+              child: Column(
+                crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(isMe ? "나" : "대동이", style: TextStyle(fontWeight: FontWeight.bold),),
+                  // for (String message in messages)
+                  Container(
+                    child: Text(message,style: TextStyle(color: isMe ? Colors.white : Colors.black, fontWeight: FontWeight.w500),),
+                    padding: EdgeInsets.all(7.0),
+                    decoration: BoxDecoration(
+                        color: isMe ? Colors.lightBlueAccent : Colors.grey.shade400,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                          bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+                          bottomLeft: isMe ? Radius.circular(12) : Radius.circular(0),
+                        ),
+                        border : Border.all(color: isMe ? Colors.lightBlueAccent : Colors.grey.shade200,)
+                    ),
+                  ),
+                  // if (message.isNotEmpty)
+                  //   // 답변이 비어 있지 않은 경우에만 표시
+                  //   Container(
+                  //     child: Text(message, style: TextStyle(color: isMe ? Colors.white : Colors.black, fontWeight: FontWeight.w500),),
+                  //     padding: EdgeInsets.all(7.0),
+                  //     decoration: BoxDecoration(
+                  //       color: isMe ? Colors.lightBlueAccent : Colors.grey.shade400,
+                  //       borderRadius: BorderRadius.only(
+                  //         topLeft: Radius.circular(12),
+                  //         topRight: Radius.circular(12),
+                  //         bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+                  //         bottomLeft: isMe ? Radius.circular(12) : Radius.circular(0),
+                  //       ),
+                  //       border: Border.all(color: isMe ? Colors.lightBlueAccent : Colors.grey.shade200,),
+                  //     ),
+                  //   ),
+                ],
+              )
+          ),
+        ],
+      ),
+    );
+    // ),
+    // );
   }
-
-
 }
-
--- daedong3/lib/view/home_page.dart
+++ daedong3/lib/view/home_page.dart
@@ -1,149 +1,205 @@
-import 'dart:js_util';
-
+import 'package:daedong3/model/chat_room.dart';
 import 'package:daedong3/view/chat/chat_message.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_mongodb_realm/database/database.dart';
 import 'package:intl/intl.dart';
 import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import '../model/chat_item.dart';
-import '../repository/repository.dart';
 import '../viewmodel/chat_view_model.dart';
 import 'hamburger/hamburger_menu.dart';
 
-class HomePage extends StatelessWidget{
-  final GlobalKey<AnimatedListState> _animListKey = GlobalKey<AnimatedListState>();
-  final messageController = TextEditingController();
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<AnimatedListState> _animListKey =
+  GlobalKey<AnimatedListState>();
   final ScrollController _scrollController = ScrollController();
+
+  final messageController = TextEditingController();
+
+  List<String> question = [];
+
+  List<String> answer = [];
+
+  List<String> message = [];
 
   List<ChatItem> chatItems = [];
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if(_scrollController.hasClients){
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    }
+
+    SchedulerBinding.instance!.addPostFrameCallback((timeStamp) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    });
+
     ChatViewModel chatViewModel = Provider.of<ChatViewModel>(context);
 
-    Logger().d("현재 채팅방 제목 = ${chatViewModel.selectedChatRoom.chatTitle}");
+    Logger().d(chatViewModel.selectedChatRoom.chatTitle);
+
+    int contextIdx = 0;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightBlueAccent,
-        title: Text("대동이",style: TextStyle(
-            fontSize: 20
-        )),
+        title: Text("대동이", style: TextStyle(fontSize: 20)),
         centerTitle: true,
-        elevation: 0.0 ,
-
-          ),
-          body: WillPopScope(
-            onWillPop: () async {
-              return false;
-            },
-            child: Center(
-              child: Column(
-
-                children: [
-                  Text("Today, ${DateFormat("Hm").format(DateTime.now())}",style: TextStyle(
-                      fontSize: 15
-                  ),
-                  ),
-                  Center(
-                    child: Container(
-                      padding:  EdgeInsets.only(top: 15,bottom: 10),
-                    ),
-                  ),
-                  Expanded(
-                    child: AnimatedList(
-                      key: _animListKey,
-                      reverse: true,
-                      itemBuilder: _buildItem,
-                    ),
-                  ),
-                  Divider(
-                    height: 5,
-                    color: Colors.lightBlueAccent,
-                  ),
-                  Container(
-                    child: ListTile(
-                      title: Container(
-                        height: 35, decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          color : Color.fromRGBO(220, 220, 220, 1)
-                      ),
-                        padding: EdgeInsets.only(left : 15),
-                        child: TextFormField(
-                          maxLines: null,
-                          controller: messageController,
-                          decoration: InputDecoration(
-                            hintText: "메세지를 입력하세요",
-                            hintStyle: TextStyle(
-                                color: Colors.black26
-                            ),
-                            border: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            errorBorder: InputBorder.none,
-                            disabledBorder: InputBorder.none,
-                          ),
-                          //onFieldSubmitted: _handleSunbmitted //모바일 어플리케이션에서의 키패트 전송버튼 활용
-                          style: TextStyle(
-                              fontSize: 16,
-                              color : Colors.black
-                          ),
-                        ),
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.send, size: 30, color: Colors.lightBlueAccent,
-                        ),
-                        onPressed: (){
-                          if(messageController.text.isEmpty){
-                            print("메세지를 입력하세요.");
-                          }
-                          else{
-                            _handleSubmitted(messageController.text, context);
-                            chatViewModel.sendMessage(chatViewModel.selectedChatRoom.id, messageController.text);
-                            messageController.clear();
-                          }
-                        },
-                      ),
-                    ),
-                  )
-                ],
+        elevation: 0.0,
+      ),
+      body: WillPopScope(
+        onWillPop: () async {
+          return false;
+        },
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                "Today, ${DateFormat("Hm").format(DateTime.now())}",
+                style: TextStyle(fontSize: 15),
               ),
-            ),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.only(top: 15, bottom: 10),
+                ),
+              ),
+              StreamBuilder(
+                  stream: chatViewModel.chatRoomController.stream,
+                  builder: (context, snapshot) {
+                    return Expanded(
+                      child: ListView.builder(
+                          controller: _scrollController,
+                          itemCount:
+                          // chatViewModel.selectedChatRoom.contextUser.length * 2,
+                          snapshot.data!.contextUser.length*2,
+                          itemBuilder: (BuildContext context, index) {
+                            final contextIdx = index ~/ 2;
+                            if (snapshot.hasError){
+                              return Text("데이터 오류");
+                            }
+                            if (snapshot.connectionState == ConnectionState.waiting){
+                              return Text("응답을 받아오는 중입니다");
+                            }
+                            // if(chatViewModel.selectedChatRoom.contextUser[contextIdx]! == null) {
+                            //   return ChatMessage("응답을 받아오는 중입니다.", false);
+                            // }
+                            if (index % 2 == 0) {
+                              // return ChatMessage(chatViewModel.selectedChatRoom.contextUser[contextIdx]['question'], true);
+                              return ChatMessage(snapshot.data!.contextUser[contextIdx]['question'], true);
+
+                            } else if (index % 2 == 1) {
+
+                              return ChatMessage(snapshot.data!.contextUser[contextIdx]['answer'], false);
+                            }
+                          }
+
+                        // child: AnimatedList(
+                        //   key: _animListKey,
+                        //   reverse: true,
+                        //   itemBuilder: _buildItem,
+                        // ),
+                      ),
+                    );
+                  }
+              ),
+              Divider(
+                height: 5,
+                color: Colors.lightBlueAccent,
+              ),
+              Container(
+                child: ListTile(
+                  title: Container(
+                    height: 35,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        color: Color.fromRGBO(220, 220, 220, 1)),
+                    padding: EdgeInsets.only(left: 15),
+                    child: TextFormField(
+                      maxLines: null,
+                      controller: messageController,
+                      decoration: InputDecoration(
+                        hintText: "메세지를 입력하세요",
+                        hintStyle: TextStyle(color: Colors.black26),
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                      ),
+                      //onFieldSubmitted: _handleSunbmitted //모바일 어플리케이션에서의 키패트 전송버튼 활용
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.send,
+                      size: 30,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    onPressed: () {
+                      if (messageController.text.isEmpty) {
+                        print("메세지를 입력하세요.");
+                      } else {
+                        _handleSubmitted(messageController.text, context);
+                        messageController.clear();
+                      }
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
+      ),
       drawer: HamburgerMenu(),
     );
   }
 
-  Widget _buildItem(context, index, animation){ // 위젯 위에 채팅을 그려주는 아이템빌더
-    // return ChatMessage(_chats[index], animation:animation);
-    ChatItem chatItem = chatItems[index];
-
-    return ChatMessage(chatItems, true, animation: animation);
-
-
-  }
-
-  void _handleSubmitted(String text, BuildContext context){
+  // Widget _buildItem(context, index, animation){ // 위젯 위에 채팅을 그려주는 아이템빌더
+  void _handleSubmitted(String text, BuildContext context) async {
+    // 메시지 제출 함수
     Logger().d(text);
 
-    ChatViewModel chatViewModel = Provider.of<ChatViewModel>(context, listen: false);
+    ChatViewModel chatViewModel =
+    Provider.of<ChatViewModel>(context, listen: false);
     // messageController.clear();
     // _chats.insert(0,text); // 유저 context의 question 집합들
-    chatViewModel.requestMessage = text;
+    await chatViewModel.sendMessage(
+        chatViewModel.selectedChatRoom.id, text);
 
-    // if (chatViewModel.requestMessage != null && chatViewModel.responseMessage != "") {
-    ChatItem newItem = ChatItem(chatViewModel.requestMessage, chatViewModel.responseMessage);
-    chatItems.insert(0, newItem);
+    await chatViewModel.requestChatRoomInfo(chatRoomId: chatViewModel.selectedChatRoom.id);
 
-
-    chatViewModel.notifyListeners();
-
-
-    _scrollController.animateTo(
-      0.0,
-      duration: Duration(milliseconds: 300),
-      curve: Curves.easeOut,
-    );
-    _animListKey.currentState?.insertItem(0);
+    // // if (chatViewModel.requestMessage != null && chatViewModel.responseMessage != "") {
+    // ChatItem newItem =
+    //     ChatItem(chatViewModel.requestMessage, chatViewModel.responseMessage);
+    // chatItems.insert(0, newItem);
+    //
+    // chatViewModel.notifyListeners();
+    //
+    // _scrollController.animateTo(
+    //   0.0,
+    //   duration: Duration(milliseconds: 300),
+    //   curve: Curves.easeOut,
+    // );
+    // _animListKey.currentState?.insertItem(0);
   }
 }
